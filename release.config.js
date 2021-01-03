2bc76e97ca2aeb87ca386348e7783b09bc0e1ac2
Szymon Nowicki <sznowicki@eskalacja.com>
2021-01-03 18:06:38 +0100

    ci: implement semrel

---
+++ release.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  branches: [
+    'master',
+    {
+      name: 'next',
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    ['@semantic-release/release-notes-generator', {
+      preset: 'conventionalcommits',
+    }],
+    ['@semantic-release/changelog', {
+      changelogFile: 'CHANGELOG.md',
+    }],
+    [
+      '@semantic-release/git', {
+        assets: [
+          'CHANGELOG.md',
+          'package.json',
+        ],
+      },
+    ],
+  ],
+};
