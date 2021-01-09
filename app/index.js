e6ab930f0b28f0ea29b2bf40923635d00c60e617
Szymon Nowicki <sznowicki@eskalacja.com>
2021-01-09 18:45:50 +0100

    feat: introduce APP_TIMEOUT env

--- app/index.js
+++ app/index.js
@@ -5,8 +5,17 @@ const main = async () => {
   const rootUrl = getEnv('APP_ROOT_URL');
   const verbose = !!getEnv('APP_VERBOSE', { defaultValue: false });
   const sleepTime = parseInt(getEnv('APP_SLEEP_TIME', { defaultValue: 25 }), 10);
+  // Default timeout 10 minutes.
+  const timeout = parseInt(getEnv('APP_TIMEOUT', { defaultValue: 600000 }), 10);
+
+  const killSwitchTimeout = setTimeout(() => {
+    console.error(`Run took too long. Timed out (${timeout}ms)`);
+    process.exit(1);
+  }, timeout);
 
   await scan(rootUrl, { verbose, sleepTime });
+
+  clearTimeout(killSwitchTimeout);
 };
 
 main();
